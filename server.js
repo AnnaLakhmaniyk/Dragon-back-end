1c0fb0215909ca784d69794c9a75ce3255ae9bc3
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-09-22 16:09:40 +0300

    add server

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require("./app");
+require("dotenv").config();
+
+const { connectMongo } = require("./src/db/conection");
+
+async function startApp() {
+  try {
+    await connectMongo();
+    app.listen(3068, () => {
+      console.log("Server running. Use our API on port: 3068");
+    });
+  } catch (error) {
+    console.log(error.message);
+
+    process.exit(1);
+  }
+}
+startApp();
